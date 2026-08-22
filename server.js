94b9046de72a27be689f7fa2b154c8f8bf0a0fc8
i50685837-sketch <i50685837@gmail.com>
2026-08-22 22:07:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 
 const connectDB = require('./config/db');
